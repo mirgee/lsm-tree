ac70b0dbcd2fdb3dafa279f8d33aaf8133776a0a
Miroslav Kovar <miroslavkovar@protonmail.com>
2024-03-05 11:30:07 +0100

    Week 1 day 3 task 2
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- src/block/iterator.rs
+++ src/block/iterator.rs
@@ -5,7 +5,10 @@ use std::sync::Arc;
 
 use bytes::Buf;
 
-use crate::key::{KeySlice, KeyVec};
+use crate::{
+    constants::SIZEOF_U16,
+    key::{KeySlice, KeyVec},
+};
 
 use super::Block;
 
@@ -36,12 +39,16 @@ impl BlockIterator {
 
     /// Creates a block iterator and seek to the first entry.
     pub fn create_and_seek_to_first(block: Arc<Block>) -> Self {
-        unimplemented!()
+        let mut new = Self::new(block);
+        new.seek_to_first();
+        new
     }
 
     /// Creates a block iterator and seek to the first key that >= `key`.
     pub fn create_and_seek_to_key(block: Arc<Block>, key: KeySlice) -> Self {
-        unimplemented!()
+        let mut new = Self::new(block);
+        new.seek_to_key(key);
+        new
     }
 
     /// Returns the key of the current entry.
@@ -65,28 +72,66 @@ impl BlockIterator {
 
     /// Seeks to the first key in the block.
     pub fn seek_to_first(&mut self) {
-        let first_key_len = self.block.data.get(0..2).unwrap().get_u16() as usize;
-        let first_key = self.block.data.get(2..2 + first_key_len).unwrap();
-        let first_value_len = self
-            .block
-            .data
-            .get(2 + first_key_len..4 + first_key_len)
-            .unwrap()
-            .get_u16() as usize;
-        let first_value = self.block.data.get(4 + first_key_len..4 + first_key_len + first_value_len).unwrap();
-        self.idx = 4 + first_key_len + first_value_len;
-        todo!()
+        self.seek_to_idx(0);
+    }
+
+    /// Modifies idx, self.key and self.value_range
+    fn seek_to_idx(&mut self, idx: usize) {
+        // As per the book: "If we reach the end of the block, we can set key to empty and return false from is_valid,
+        // so that the caller can switch to another block if possible."
+        if idx >= self.block.offsets.len() {
+            self.key.clear();
+            self.value_range = (0, 0);
+            return;
+        }
+        let offset = self.block.offsets[idx] as usize;
+        self.seek_to_offset_bytes(offset);
+        self.idx = idx;
+    }
+
+    /// Assumes that there is a valid entry after offset_bytes
+    /// Modifies self.key and self.value_range
+    fn seek_to_offset_bytes(&mut self, offset_bytes: usize) {
+        let mut current_entry = self.block.data[offset_bytes..].as_ref();
+
+        let key_len = current_entry.get_u16() as usize;
+        let key = current_entry[..key_len].as_ref();
+        current_entry.advance(key_len);
+
+        let value_len = current_entry.get_u16() as usize;
+        let value = current_entry[..value_len].as_ref();
+        current_entry.advance(value_len);
+
+        self.key.clear();
+        self.key.append(key);
+
+        let value_range_begin = offset_bytes + SIZEOF_U16 + key_len + SIZEOF_U16;
+        let value_range_end = value_range_begin + value_len;
+        self.value_range = (value_range_begin, value_range_end);
     }
 
     /// Move to the next key in the block.
     pub fn next(&mut self) {
-        unimplemented!()
+        self.seek_to_idx(self.idx + 1);
     }
 
     /// Seek to the first key that >= `key`.
     /// Note: You should assume the key-value pairs in the block are sorted when being added by
     /// callers.
     pub fn seek_to_key(&mut self, key: KeySlice) {
-        unimplemented!()
+        let mut left = 0;
+        let mut right = self.block.offsets.len();
+        while left < right {
+            let middle = left + (right - left) / 2;
+            self.seek_to_idx(middle);
+            if self.key() > key {
+                right = middle;
+            } else if self.key() < key {
+                left = middle + 1;
+            } else {
+                return;
+            }
+        }
+        self.seek_to_idx(left);
     }
 }
