0a82076ea5581179f9c56170100b21df8a3ee4d3
Miroslav Kovar <miroslavkovar@protonmail.com>
2024-03-09 10:28:02 +0100

    Week 1 day 5 task 1
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- src/iterators/two_merge_iterator.rs
+++ src/iterators/two_merge_iterator.rs
@@ -10,7 +10,7 @@ use super::StorageIterator;
 pub struct TwoMergeIterator<A: StorageIterator, B: StorageIterator> {
     a: A,
     b: B,
-    // Add fields as need
+    use_a: bool,
 }
 
 impl<
@@ -18,8 +18,34 @@ impl<
         B: 'static + for<'a> StorageIterator<KeyType<'a> = A::KeyType<'a>>,
     > TwoMergeIterator<A, B>
 {
+    fn use_a(a: &A, b: &B) -> bool {
+        if !a.is_valid() {
+            return false;
+        }
+        if !b.is_valid() {
+            return true;
+        }
+        a.key() <= b.key()
+    }
+
+    // Similar to MergeIterator, if the same key is found in both iterators, a takes the precedence. So, we only ever need to skip b.
+    fn skip_b(a: &A, b: &mut B) -> Result<()> {
+        // Accessing the key may give incrorrect result if the iterator is invalid
+        if a.is_valid() && b.is_valid() && a.key() == b.key() {
+            b.next()?;
+        };
+        Ok(())
+    }
+
     pub fn create(a: A, b: B) -> Result<Self> {
-        unimplemented!()
+        let mut new = Self {
+            use_a: false,
+            a,
+            b,
+        };
+        Self::skip_b(&new.a, &mut new.b)?;
+        new.use_a = Self::use_a(&new.a, &new.b);
+        Ok(new)
     }
 }
 
@@ -31,18 +57,37 @@ impl<
     type KeyType<'a> = A::KeyType<'a>;
 
     fn key(&self) -> Self::KeyType<'_> {
-        unimplemented!()
+        if self.use_a {
+            self.a.key()
+        } else {
+            self.b.key()
+        }
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        if self.use_a {
+            self.a.value()
+        } else {
+            self.b.value()
+        }
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        if self.use_a {
+            self.a.is_valid()
+        } else {
+            self.b.is_valid()
+        }
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        if self.use_a {
+            self.a.next()?;
+        } else {
+            self.b.next()?;
+        }
+        Self::skip_b(&self.a, &mut self.b)?;
+        self.use_a = Self::use_a(&self.a, &self.b);
+        Ok(())
     }
 }
