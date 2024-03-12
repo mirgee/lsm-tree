5864c3ac3f808219ff8c678dd817093043e5d13b
Miroslav Kovar <miroslavkovar@protonmail.com>
2024-03-12 11:54:50 +0100

    Week 1 day 7 task 1
    Signed-off-by: Miroslav Kovar <miroslavkovar@protonmail.com>

--- src/table/bloom.rs
+++ src/table/bloom.rs
@@ -79,7 +79,15 @@ impl Bloom {
         let mut filter = BytesMut::with_capacity(nbytes);
         filter.resize(nbytes, 0);
 
-        // TODO: build the bloom filter
+        for key in keys {
+            let mut key = *key;
+            let delta = (key >> 17) | (key << 15);
+            for _ in 0..k {
+                let bit_to_set = (key as usize) % nbits;
+                filter.set_bit(bit_to_set, true);
+                key = key.wrapping_add(delta);
+            }
+        }
 
         Self {
             filter: filter.freeze(),
@@ -88,7 +96,7 @@ impl Bloom {
     }
 
     /// Check if a bloom filter may contain some data
-    pub fn may_contain(&self, h: u32) -> bool {
+    pub fn may_contain(&self, mut h: u32) -> bool {
         if self.k > 30 {
             // potential new encoding for short bloom filters
             true
@@ -96,7 +104,13 @@ impl Bloom {
             let nbits = self.filter.bit_len();
             let delta = (h >> 17) | (h << 15);
 
-            // TODO: probe the bloom filter
+            for _ in 0..self.k {
+                let bit_to_check = (h as usize) % nbits;
+                if !self.filter.get_bit(bit_to_check) {
+                    return false;
+                }
+                h = h.wrapping_add(delta);
+            }
 
             true
         }
--- src/tests.rs
+++ src/tests.rs
@@ -6,5 +6,6 @@ mod week1_day1;
 mod week1_day3;
 mod week1_day6;
 mod week1_day2;
+mod week1_day7;
 mod week1_day4;
 mod harness;
